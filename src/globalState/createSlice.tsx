d6e52d97e2cb1927398c7e0a88146a1fb7a97917
waqasansarii <65023462+waqasansarii@users.noreply.github.com>
2021-01-11 20:57:45 +0500

    changed map into filter function

--- src/globalState/createSlice.tsx
+++ src/globalState/createSlice.tsx
@@ -8,19 +8,19 @@ const firtSlice = createSlice({
 
 
   reducers: {
-    addItem: (state: any, action) => {
+    addItem: (state, action:any) => {
            
-      Products.list.map((val)=>{
+      Products.list.filter((val)=>{
         if(val.id===action.payload.id){
-       return val.bool=true
+        val.bool=true
+        
       }})
-
       state.basket = [action.payload,...state.basket]
     },
     deleteItem: (state, action) => {
 
-      Products.list.map((val)=>{if(val.id===action.payload.id){
-      return  val.bool=false
+      Products.list.filter((val)=>{if(val.id===action.payload.id){
+        val.bool=false
       }})
 
       let filter: any = state.basket.filter((val) => val.id !== action.payload.id)
